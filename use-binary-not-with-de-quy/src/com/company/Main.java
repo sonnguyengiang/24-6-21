de75b4b78825d71dd53d2951bb9930330e3706b0
sonnguyengiang <sonstarx2212@gmail.com>
2021-06-24 16:33:11 +0700

    yep

---
+++ use-binary-not-with-de-quy/src/com/company/Main.java
@@ -0,0 +1,9 @@
+package com.company;
+
+public class Main {
+
+    public static void main(String[] args) {
+        int x = BinarySearch.binarySearch(BinarySearch.list, 79);
+        System.out.println(x);
+    }
+}
